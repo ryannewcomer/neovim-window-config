046645cfb3972af683a70188de1e394ee58c5f08
Ryan Hung <ryanhungryan@gmail.com>
2026-05-04 13:28:01 +0100

    added keybland for lsp diagnostics

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,16 @@
 return {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		keys = {
-				{ "<leader>f", "<cmd>Telescope find_files<cr>", desc="find file"},
-				{ "<leader>c", "<cmd>Telescope colorscheme<cr>", desc="colorscheme"}
-		}
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>f", "<cmd>Telescope find_files<cr>", desc = "find file" },
+		{ "<leader>c", "<cmd>Telescope colorscheme<cr>", desc = "colorscheme" },
+		{
+			"<leader>d",
+			function()
+				local builtin = require("telescope.builtin")
+				builtin.diagnostics()
+			end,
+		},
+	},
 }
